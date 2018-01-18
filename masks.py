00b51bc64c28f34655ad2e3299de0c2276a07c39
Thierry DECKER <mail@thierry-decker.com>
2018-01-18 14:54:39 +0100

    Updated masks.py

--- masks.py
+++ masks.py
@@ -13,5 +13,60 @@
 #
 # Si le masque donné en notation décimale est invalide, retourner None.
 #
-# Vous n'etes pas autorisé à utiliser l'instruction import.
+# Vous n'etes pas autorisés à utiliser l'instruction import.
 #
+
+def decimal_to_cidr(decimal_mask):
+    """Calcul du masque CIDR à partir du masque décimal
+    inputs:
+        - Masque décimal
+    outputs:
+        - Masque CIDR
+    """
+    cidr_mask = None
+
+    # Tests de validité du masque décimal
+    if not isinstance(decimal_mask, str):
+        print("A string is needed")
+        return
+
+    splitted_dm = list(decimal_mask.split(sep='.'))
+
+    if len(splitted_dm) != 4:
+        print("A decimal mask must have four fields")
+        return
+
+    for part in splitted_dm:
+        if not part.isalnum():
+            print("Fields must be positive integers")
+            return
+
+    for i in range(len(splitted_dm)):
+        splitted_dm[i] = int(splitted_dm[i])
+        if splitted_dm[i] > 255:
+            print("Fields must be positive integers less or equal to 255")
+            return
+
+    for i in range(len(splitted_dm)):
+        splitted_dm[i] = bin(splitted_dm[i])
+
+    binary_mask = ''
+    for i in range(len(splitted_dm)):
+        binary_mask += splitted_dm[i][2:].zfill(8)
+
+    number_of_ones = binary_mask.count('1')
+
+    if number_of_ones != binary_mask[0:number_of_ones].count('1'):
+        print("Invalid mask")
+        return None
+    return number_of_ones
+
+
+
+
+
+
+
+
+decimal_mask = '255.0.0.0'
+print("Le masque CIDR de {} est {}".format(decimal_mask, decimal_to_cidr(decimal_mask)))
