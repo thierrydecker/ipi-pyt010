0b4c0d81bae0592257220c4dd5a16498d80d9ec8
Thierry DECKER <mail@thierry-decker.com>
2018-01-18 16:29:23 +0100

    Added skeleton.py

---
+++ skeleton.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding="utf-8" -*-
+
+# Imports de la biblithèque standard
+#
+
+#
+# Imports des bibliothèques tierces
+#
+
+#
+# Internal functions
+#
+
+#
+# main() function
+#
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
