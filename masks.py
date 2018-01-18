34e0b85e4689e0d521463cb2c010574820ee5ce8
Thierry DECKER <mail@thierry-decker.com>
2018-01-18 18:45:15 +0100

    Optimized masks.py

--- masks.py
+++ masks.py
@@ -16,51 +16,49 @@
 # Vous n'etes pas autorisés à utiliser l'instruction import.
 #
 
-def decimal_to_cidr(decimal_mask):
+
+def decimal_to_cidr(dm):
     """Calcul du masque CIDR à partir du masque décimal
     inputs:
         - Masque décimal
     outputs:
         - Masque CIDR
     """
-    cidr_mask = None
 
     # Tests de validité du masque décimal
-    if not isinstance(decimal_mask, str):
+    if not isinstance(dm, str):
         print("A string is needed")
-        return
+        return None
 
-    splitted_dm = list(decimal_mask.split(sep='.'))
+    splitted_dm = list(dm.split(sep='.'))
 
     if len(splitted_dm) != 4:
         print("A decimal mask must have four fields")
-        return
-
-    for part in splitted_dm:
-        if not part.isalnum():
-            print("Fields must be positive integers")
-            return
+        return None
 
     for i in range(len(splitted_dm)):
+
+        if not splitted_dm[i].isalnum():
+            print("Fields must be positive integers")
+            return None
+
         splitted_dm[i] = int(splitted_dm[i])
+
         if splitted_dm[i] > 255:
             print("Fields must be positive integers less or equal to 255")
-            return
+            return None
 
-    for i in range(len(splitted_dm)):
-        splitted_dm[i] = bin(splitted_dm[i])
+        splitted_dm[i] = format(splitted_dm[i], "0>8b")
 
-    binary_mask = ''
-    for i in range(len(splitted_dm)):
-        binary_mask += splitted_dm[i][2:].zfill(8)
-
+    binary_mask = "".join(splitted_dm)
     number_of_ones = binary_mask.count('1')
 
     if number_of_ones != binary_mask[0:number_of_ones].count('1'):
         print("Invalid mask")
         return None
+
     return number_of_ones
 
 
-decimal_mask = '255.0.0.0'
+decimal_mask = '255.255.255.128'
 print("Le masque CIDR de {} est {}".format(decimal_mask, decimal_to_cidr(decimal_mask)))
