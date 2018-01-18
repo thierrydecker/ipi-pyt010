6390fdfcec1505192cf77cda460dce032cc80117
Thierry DECKER <mail@thierry-decker.com>
2018-01-18 06:29:43 +0100

    updated project structure

--- for-structs.py
+++ for-structs.py
@@ -3,28 +3,7 @@
 #
 
 #
-# Définition d'une variable et assignation
-#
-a = 1
-
-#
-# if ... else ... (si ... sinon ...)
-#
-if a == 10:
-    print("ma variable est égale à 10")
-else:
-    print("ma variable n'est pas égale à 10")
-
-#
-# if ... elif ... (si ... sinon si ...)
-#
-if a == 10:
-    print("ma variable est égale à 10")
-elif a == 1:
-    print("ma variable n'est pas égale à 10")
-
-#
-# Boucles
+# Boucles for
 #
 
 #
@@ -81,7 +60,7 @@ pairs = tuple(pairs)
 print("Le tuple contient ->", pairs)
 
 #
-# Version 2
+# Version 2 (Comprehension list)
 #
 print("Le tuple contient ->", tuple([i for i in range(0, 2 * nb, 2)]))
 print("Le tuple contient ->", tuple([i * 2 for i in range(0, nb)]))
@@ -91,6 +70,10 @@ print("Le tuple contient ->", tuple([i * 2 for i in range(0, nb)]))
 #
 # Créer par un boucle la liste suivante
 # [ ['A',1], ['B',2], ... , ['Z',26] ]
+#
+
+#
+# Correction
 #
 debut, fin = 'A', 'z'
 maListe = [[chr(i), ord(chr(i)) - ord(debut) + 1] for i in range(ord(debut), ord(fin) + 1)]
---
+++ ifs-structs.py
@@ -0,0 +1,24 @@
+#
+# structures de controle
+#
+
+#
+# Définition d'une variable et assignation
+#
+a = 1
+
+#
+# if ... else ... (si ... sinon ...)
+#
+if a == 10:
+    print("ma variable est égale à 10")
+else:
+    print("ma variable n'est pas égale à 10")
+
+#
+# if ... elif ... (si ... sinon si ...)
+#
+if a == 10:
+    print("ma variable est égale à 10")
+elif a == 1:
+    print("ma variable n'est pas égale à 10")
