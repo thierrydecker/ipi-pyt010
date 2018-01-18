cd4441293d98c2c7a42987de7688715b7ac6ff58
Thierry DECKER <mail@thierry-decker.com>
2018-01-18 13:41:48 +0100

    Added masks.py

---
+++ masks.py
@@ -0,0 +1,17 @@
+#
+# Exercice sur les fonctions
+#
+# Initialiser une variable chaine de caractères contenant un masque réseau
+# en notation décimale (aaa.bbb.ccc.ddd).
+#
+# Ecrire une ou plusieures fonctions permettant de convertir le masque décimal
+# dans son équivalent CIDR
+#
+# Exemple : 255.255.255.0   doit donner /24
+#           255.255.0.0     doit donner /16
+#           255.255.255.252 doit donner /30
+#
+# Si le masque donné en notation décimale est invalide, retourner None.
+#
+# Vous n'etes pas autorisé à utilisé l'instruction import.
+#
