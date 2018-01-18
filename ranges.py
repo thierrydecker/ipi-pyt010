ddf23d15c5b0683f84e7ce41d4cdfa76512f85d1
Thierry DECKER <mail@thierry-decker.com>
2018-01-18 06:52:23 +0100

    Added ranges

---
+++ ranges.py
@@ -0,0 +1,32 @@
+import random
+
+#
+# Exercices sur les ranges
+#
+
+#
+# Afficher le compte à rebours d'un entier à un autre
+#
+# Correction
+#
+debut, fin = 20, 0
+for i in range(debut, fin - 1, -1):
+    print("--> {}".format(i))
+
+#
+# Créer un liste d'entier et la trier en ordre décroissant
+#
+debut, fin = 10, 20
+maListe = [i for i in range(debut, fin)]
+maListe.reverse()
+print("maListe -> {}".format(maListe))
+
+#
+# Créer une liste de n entiers aléatoires (compris) en <debut> et <fin>) et la trier en
+# ordre croissant. L'afficher avant et après le tri
+#
+nb, debut, fin = 10, 0, 10001
+maListe = [random.randrange(debut, fin) for i in range(nb + 1)]
+print("maListe avant tri -> {}".format(maListe))
+maListe.sort()
+print("maListe après tri -> {}".format(maListe))
